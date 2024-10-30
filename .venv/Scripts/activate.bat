9e5879ad94363729cd62836fc0d8baa9a2fe4e6c
prathambahekar <prathambahekar97@gmail.com>
2024-10-30 13:23:55 +0530

    venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=d:\Codes\king-of-diamonds\.venv
+set VIRTUAL_ENV=c:\Users\prath\OneDrive\Desktop\venv\king-of-diamonds\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
